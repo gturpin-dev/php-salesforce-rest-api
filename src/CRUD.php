220884f92606c95731548d1a454ffbf79d85ad62
Gturpin <34162504+gturpin-dev@users.noreply.github.com>
2021-08-09 11:05:41 +0200

    Adding Version control on CRUD object

--- src/CRUD.php
+++ src/CRUD.php
@@ -3,31 +3,40 @@
 namespace bjsmasth\Salesforce;
 
 use GuzzleHttp\Client;
-use Exception\Salesforce as SalesforceException;
 
 class CRUD
 {
     protected $instance_url;
     protected $access_token;
+	private   $api_version;
 
     public function __construct()
     {
         if (!isset($_SESSION) and !isset($_SESSION['salesforce'])) {
-            throw new SalesforceException('Access Denied', 403);
+            throw new \Exception('Access Denied', 403);
         }
 
         $this->instance_url = $_SESSION['salesforce']['instance_url'];
         $this->access_token = $_SESSION['salesforce']['access_token'];
+        $this->api_version  = 'v52.0';
     }
+
+	public function getApiVersion() {
+		return $this->api_version;
+	}
+
+	public function setApiVersion(string $api_version) {
+		$this->api_version = $api_version;
+	}
 
     public function query($query)
     {
-        $url = "{$this->instance_url}/services/data/v39.0/query";
+        $url = "$this->instance_url/services/data/$this->api_version/query";
 
         $client = new Client();
         $request = $client->request('GET', $url, [
             'headers' => [
-                'Authorization' => "OAuth {$this->access_token}"
+                'Authorization' => "OAuth $this->access_token"
             ],
             'query' => [
                 'q' => $query
@@ -39,13 +48,13 @@ class CRUD
 
     public function create($object, array $data)
     {
-        $url = "{$this->instance_url}/services/data/v39.0/sobjects/{$object}/";
+        $url = "$this->instance_url/services/data/$this->api_version/sobjects/$object/";
 
         $client = new Client();
 
         $request = $client->request('POST', $url, [
             'headers' => [
-                'Authorization' => "OAuth {$this->access_token}",
+                'Authorization' => "OAuth $this->access_token",
                 'Content-type' => 'application/json'
             ],
             'json' => $data
@@ -54,9 +63,7 @@ class CRUD
         $status = $request->getStatusCode();
 
         if ($status != 201) {
-            throw new SalesforceException(
-                "Error: call to URL {$url} failed with status {$status}, response: {$request->getReasonPhrase()}"
-            );
+            die("Error: call to URL $url failed with status $status, response: " . $request->getReasonPhrase());
         }
 
         $response = json_decode($request->getBody(), true);
@@ -68,7 +75,7 @@ class CRUD
 
     public function update($object, $id, array $data)
     {
-        $url = "{$this->instance_url}/services/data/v39.0/sobjects/{$object}/{$id}";
+        $url = "$this->instance_url/services/data/$this->api_version/sobjects/$object/$id";
 
         $client = new Client();
 
@@ -83,34 +90,7 @@ class CRUD
         $status = $request->getStatusCode();
 
         if ($status != 204) {
-            throw new SalesforceException(
-                "Error: call to URL {$url} failed with status {$status}, response: {$request->getReasonPhrase()}"
-            );
-        }
-
-        return $status;
-    }
-
-    public function upsert($object, $field, $id, array $data)
-    {
-        $url = "{$this->instance_url}/services/data/v39.0/sobjects/{$object}/{$field}/{$id}";
-
-        $client = new Client();
-
-        $request = $client->request('PATCH', $url, [
-            'headers' => [
-                'Authorization' => "OAuth {$this->access_token}",
-                'Content-type' => 'application/json'
-            ],
-            'json' => $data
-        ]);
-
-        $status = $request->getStatusCode();
-
-        if ($status != 204 && $status != 201) {
-            throw new SalesforceException(
-                "Error: call to URL {$url} failed with status {$status}, response: {$request->getReasonPhrase()}"
-            );
+            die("Error: call to URL $url failed with status $status, response: " . $request->getReasonPhrase());
         }
 
         return $status;
@@ -118,21 +98,19 @@ class CRUD
 
     public function delete($object, $id)
     {
-        $url = "{$this->instance_url}/services/data/v39.0/sobjects/{$object}/{$id}";
+        $url = "$this->instance_url/services/data/$this->api_version/sobjects/$object/$id";
 
         $client = new Client();
         $request = $client->request('DELETE', $url, [
             'headers' => [
-                'Authorization' => "OAuth {$this->access_token}",
+                'Authorization' => "OAuth $this->access_token",
             ]
         ]);
 
         $status = $request->getStatusCode();
 
         if ($status != 204) {
-            throw new SalesforceException(
-                "Error: call to URL {$url} failed with status {$status}, response: {$request->getReasonPhrase()}"
-            );
+            die("Error: call to URL $url failed with status $status, response: " . $request->getReasonPhrase());
         }
 
         return true;
